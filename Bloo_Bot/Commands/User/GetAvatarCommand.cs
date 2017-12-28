2923c1a3057e9cfb510058b273e251b1a8685dd9
James Johnson <music1384@yahoo.co.uk>
2017-12-28 09:50:14 +0000

    Implement retrieval of user avatars

---
+++ Bloo_Bot/Commands/User/GetAvatarCommand.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+
+namespace BlooBot.Commands.User
+{
+    class GetAvatarCommand
+    {
+        public async Task GetAvatar (CommandContext ctx, DiscordMember member)
+        {
+            await ctx.RespondAsync ("", embed: await GetEmbed (ctx, member));
+        }
+
+        private async Task<DiscordEmbed> GetEmbed (CommandContext ctx, DiscordMember member)
+        {
+            var footers = await GetFooters ();
+            string title = $"{ctx.User.Username} requested { member.Username} 's avatar! ^^";
+
+            var response = new DiscordEmbedBuilder ()
+            {
+                Title = title,
+                Color = DiscordColor.Azure,
+                ImageUrl = member.AvatarUrl,
+                Footer = new DiscordEmbedBuilder.EmbedFooter ()
+                {
+                    Text = footers[Bloo._Rand.Next (0, footers.Length)]
+                }
+            };
+
+            return response;
+        }
+
+        private async Task<string[]> GetFooters ()
+        {
+            await Task.Yield ();
+
+            return new string[]
+                 {
+                    "Looks pretty good!",
+                    "Aw yee. I like this one.",
+                    "Well... I wonder they did that. -.^",
+                    "Ooh! Coot."
+                };
+        }
+    }
+}
