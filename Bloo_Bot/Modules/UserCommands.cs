dd88d73af230ec47964d2fd3b25373f43dbc4bf6
James Johnson <music1384@yahoo.co.uk>
2017-12-28 09:49:56 +0000

    Template user commands ready for implementation

---
+++ Bloo_Bot/Modules/UserCommands.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace BlooBot.Commands
+{
+    [Group ("user")]
+    [Description ("Commands for user profiles")]
+    class UserCommands
+    {
+        [Command ("avatar")]
+        [Description ("Grabs a mentioned users avatar.")]
+        [Aliases ("a")]
+        public async Task GetAvatar (CommandContext ctx, [Description ("The user you want the avatar of.")] DiscordMember member)
+        {
+            await ctx.TriggerTypingAsync ();
+
+            try
+            {
+                await new User.GetAvatarCommand ().GetAvatar (ctx, member);
+            }
+            catch (Exception e)
+            {
+                await ctx.RespondAsync ("Sorry! Seems like I can't do that right now.\nError: " + e.Message);
+            }
+        }
+
+        [Command ("user")]
+        [Description ("Grabs a user and all of their information.")]
+        [Aliases ("u")]
+        public async Task GetUser (CommandContext ctx, [Description ("The discord member to grab")] DiscordMember member)
+        {
+            await ctx.TriggerTypingAsync ();
+
+            try
+            {
+                await new User.GetUserInfoCommand ().GetUser (ctx, member);
+            }
+            catch (Exception e)
+            {
+                await ctx.RespondAsync (e.Message);
+            }
+        }
+    }
+}
